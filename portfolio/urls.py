48e6655364dd4e510a70b19ee4797509debac8f5
Hamza <hamzabhoja14@gmail.com>
2023-06-08 11:56:47 +0200

    Added documentation using Sphinx

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# This code is defining the URL patterns for a Django web application. It is creating two URL
+# patterns:
 urlpatterns = [
     path('', views.home_view, name="home"),
     path('register/', views.register, name='register'),
--- portfolio/views.py
+++ portfolio/views.py
@@ -5,6 +5,13 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
 def register(request):
+    """
+    This function registers a user by creating a form and saving the user's information if the form is
+    valid.
+    
+    :param: The request object represents the HTTP request that the user made to the server. It contains information about the request, such as the HTTP method used (GET, POST, etc.), the URL requested, and any data submitted in the request
+    :return: The function `register` returns a rendered HTML template `register.html` with a `UserCreationForm` object passed as context if the request method is GET. If the request method is POST and the form is valid, the function redirects to the `home` URL. If the form is not valid, the same HTML template with the invalid form is returned.
+    """
     if request.POST:
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -18,4 +25,10 @@ def register(request):
 
 @login_required()
 def home_view(request):
+    """
+    This is a Python function that returns a rendered HTML template called "index.html" for a home view.
+    
+    :param: The request parameter is an object that represents the HTTP request made by the client to the server. It contains information such as the HTTP method used (GET, POST, etc.), the headers, the query parameters, and the body of the request. It is passed as the first argument to Django views
+    :return: The `home_view` function is returning an HTTP response that renders the `index.html` template using the `render` function.
+    """
     return render(request, 'index.html')
